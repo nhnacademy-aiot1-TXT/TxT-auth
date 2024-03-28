8459731a5deceba27ef7864360c2ee09f771d873
woooyas <njmin8@naver.com>
2024-03-28 11:11:16 +0900

    Feat: dto 추가

---
+++ src/main/java/com/nhnacademy/aiot/authentication/dto/AccessTokenResponse.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.aiot.authentication.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AccessTokenResponse {
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+    @JsonProperty("expire_in")
+    private Integer expiresIn;
+}
