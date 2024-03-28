ec8c006eaf0dde98792d44ddd9c5bdd70e70e324
woooyas <njmin8@naver.com>
2024-03-28 18:25:03 +0900

    Feat: refresh token 재발급 controller 구현

---
+++ src/main/java/com/nhnacademy/aiot/authentication/controller/ReissueController.java
@@ -0,0 +1,42 @@
+package com.nhnacademy.aiot.authentication.controller;
+
+import com.nhnacademy.aiot.authentication.dto.AccessTokenResponse;
+import com.nhnacademy.aiot.authentication.exception.InvalidTokenException;
+import com.nhnacademy.aiot.authentication.service.JwtService;
+import com.nhnacademy.aiot.authentication.service.RedisService;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth/reissue")
+public class ReissueController {
+
+    private final RedisService redisService;
+    private final JwtService jwtService;
+
+    @PostMapping
+    public AccessTokenResponse reissue(@RequestHeader("X-REFRESH_TOKEN") String refreshToken) {
+        String userId;
+        try {
+            userId = jwtService.extractClaims(refreshToken).get("userId", String.class);
+        } catch (SignatureException e) {
+            userId = ((UserDetails) SecurityContextHolder.getContext()
+                                                         .getAuthentication()
+                                                         .getPrincipal()).getUsername();
+            redisService.removeUserTokens(userId);
+            log.error(userId + ": refresh token 변조가 의심되어 해당 사용자의 refresh token을 모두 삭제하였습니다.");
+            throw new InvalidTokenException();
+        }
+
+        return new AccessTokenResponse(jwtService.generateAccessToken(userId), jwtService.getPrefix(), jwtService.getAccessExpiryTime());
+    }
+}
