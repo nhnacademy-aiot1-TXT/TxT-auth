1470e15d44faaa7060085b26ab26853d4820df81
woooyas <njmin8@naver.com>
2024-03-28 11:01:55 +0900

    Chore: 라이브러리 추가

--- src/main/java/com/nhnacademy/aiot/authentication/AuthenticationApplication.java
+++ src/main/java/com/nhnacademy/aiot/authentication/AuthenticationApplication.java
@@ -2,8 +2,14 @@ package com.nhnacademy.aiot.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
