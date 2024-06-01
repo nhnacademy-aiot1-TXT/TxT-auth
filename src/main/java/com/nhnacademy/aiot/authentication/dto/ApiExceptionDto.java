013060a022fc6d24fc04eb301b06c57c2c8cb35f
jongsikk <xxx_@kakao.com>
2024-06-01 16:19:21 +0900

    Docs: ApiExceptionDto JavaDoc 작성

--- src/main/java/com/nhnacademy/aiot/authentication/dto/ApiExceptionDto.java
+++ src/main/java/com/nhnacademy/aiot/authentication/dto/ApiExceptionDto.java
@@ -4,6 +4,12 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * 예외처리 응답 DTO
+ *
+ * @author jongsikk
+ * @version 1.0.0
+ */
 @Data
 public class ApiExceptionDto {
     private final LocalDateTime time;
